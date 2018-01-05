a88c08d886948834a35b36531627abb0f8536c25
Andy Warren <mail@andrewpwarren.co.uk>
2018-01-05 16:57:34 +0000

    socket can handle multiple connections

--- socket/socket.go
+++ socket/socket.go
@@ -19,50 +19,81 @@ var upgrader = websocket.Upgrader{
 	},
 }
 
-var conn *websocket.Conn
+type Client struct {
+	// The websocket connection.
+	conn *websocket.Conn
 
-func parseMessage(m string) {
+	// Buffered channel of outbound messages.
+	send chan []byte
+}
+
+func parseMessage(m string) error {
 	parsedM, err := utils.ParseKeyValueStr(m, "=")
 	if err != nil {
-		WriteMessage(err.Error())
-		return
+		return err
 	}
 	color := parsedM.Key
-	value, err := strconv.ParseFloat(parsedM.Value, 64)
-	if err != nil {
-		WriteMessage(fmt.Sprintf("error converting %v to float: %v", value, err))
-		return
+	value, parsingErr := strconv.ParseFloat(parsedM.Value, 64)
+	if parsingErr != nil {
+		return fmt.Errorf("error converting %v to float: %v", value, parsingErr)
 	}
 	applyErr := leds.Apply(color, value)
 	if applyErr != nil {
-		WriteMessage(applyErr.Error())
+		return applyErr
 	}
+	return nil
 }
 
 // WriteMessage takes a string message (m) and sends it onto the socket connection
-func WriteMessage(m string) {
+func WriteMessage(m string, conn *websocket.Conn) {
 	byteMessage := []byte(m)
 	if err := conn.WriteMessage(1, byteMessage); err != nil {
 		log.Println(err)
 		return
 	}
 }
+func (c *Client) write() {
+	for {
+		select {
+		case message := <-c.send:
+			if err := c.conn.WriteMessage(1, message); err != nil {
+				log.Println(err)
+				return
+			}
+		}
+	}
+}
+
+func (c *Client) read() {
+	defer func() {
+		c.conn.Close()
+	}()
+	for {
+		_, message, err := c.conn.ReadMessage()
+		if err != nil {
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway) {
+				log.Printf("error: %v", err)
+			}
+			break
+		}
+		// formatted := fmt.Sprintf("message received: %v \n", string(message))
+		parseMessageError := parseMessage(string(message))
+		fmt.Println(parseMessageError)
+		if parseMessageError != nil {
+			c.send <- []byte(parseMessageError.Error())
+		}
+	}
+}
 
 // Handler takes a ResponseWriter and request and upgrades it to a socket connection
 // It reads the incoming message and parses it
 func Handler(w http.ResponseWriter, r *http.Request) {
-	connection, err := upgrader.Upgrade(w, r, nil)
+	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Println(err)
 		return
 	}
-	conn = connection
-	for {
-		_, p, err := conn.ReadMessage()
-		if err != nil {
-			log.Println(err)
-			return
-		}
-		parseMessage(string(p))
-	}
+	client := &Client{conn: conn, send: make(chan []byte, 256)}
+	go client.read()
+	go client.write()
 }
